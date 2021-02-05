482445adf0c948a74cc8b08012e67726f4466c2f
钱明圆 <1541894331@qq.com>
2021-02-05 17:41:45 +0800

    chery pick test2

--- src/main/java/com/wechat/robot/job/WeatherXxlJob.java
+++ src/main/java/com/wechat/robot/job/WeatherXxlJob.java
@@ -36,4 +36,7 @@ public class WeatherXxlJob {
         robotHttp.sendRobotMessage(WeatherHttp.CODE_WUXI);
         return ReturnT.SUCCESS;
     }
+
+    //chery pick test1
+    //chery pick test2
 }

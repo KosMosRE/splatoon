94ecd50cc661c98567a5f4f13e3b3d3ff4b32e97
钱明圆 <1541894331@qq.com>
2021-02-19 15:19:41 +0800

    综合合并 feat: 习题练习
    feat: 习题练习2

---
+++ src/main/java/com/wechat/robot/config/Merage.java
@@ -0,0 +1,10 @@
+package com.wechat.robot.config;
+
+/**
+ * 请求测试
+ *
+ * @author QianMY
+ * @date 2021-02-19 15:19
+ */
+public class Merage {
+}
---
+++ src/main/java/com/wechat/robot/config/Mrage.java
@@ -0,0 +1,10 @@
+package com.wechat.robot.config;
+
+/**
+ * ceshi2
+ *
+ * @author QianMY
+ * @date 2021-02-19 15:24
+ */
+public class Mrage {
+}
